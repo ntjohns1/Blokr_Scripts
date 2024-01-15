d15aac33f6585ad68436eb90bc04967ed4d3d339
ntjohns1 <nelsontjohns@gmail.com>
2024-01-14 22:33:11 -0500

    Setting up GetOccupiedGridPositions for each piece script

--- Assets/Scripts/Pieces/C5.cs
+++ Assets/Scripts/Pieces/C5.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class C5 : Piece
 {
-    public override List< Vector2Int> GetOccupiedGridPositions(Vector2Int gridPoint)
+    public override List<Vector2Int> GetOccupiedGridPositions(Vector2Int cellA, Direction direction)
     {
-        List<Vector2Int> spaces = new List<Vector2Int>
+        var cellB = direction switch
         {
-            gridPoint
+            Direction.Up => new Vector2Int(cellA.x + 1, cellA.y),
+            Direction.Right => new Vector2Int(cellA.x, cellA.y - 1),
+            Direction.Down => new Vector2Int(cellA.x - 1, cellA.y),
+            Direction.Left => new Vector2Int(cellA.x, cellA.y + 1),
+            _ => new Vector2Int(cellA.x + 1, cellA.y),
         };
-        return spaces;
-    }   
+        List<Vector2Int> cells = new()
+        {
+            cellA,
+            cellB
+        };
+        return cells;
+    }
 }
