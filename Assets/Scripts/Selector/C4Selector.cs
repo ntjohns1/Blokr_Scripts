27df088d4dcef5ba1603679e8f3838e3ec06ec3d
ntjohns1 <nelsontjohns@gmail.com>
2024-01-15 12:07:56 -0500

    Using newly created Selector classes and templates for other pieces

--- Assets/Scripts/Selector/C4Selector.cs
+++ Assets/Scripts/Selector/C4Selector.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class C4Selector : Selector
 {
-        public override List<Vector2Int> GetOccupiedGridPositions(Vector2Int cellA, Direction direction)
+    public override List<Vector2Int> GetOccupiedGridPositions(Vector2Int cellA, Direction direction)
     {
         var cellB = direction switch
         {
@@ -14,11 +14,29 @@ public class C4Selector : Selector
             Direction.Left => new Vector2Int(cellA.x, cellA.y + 1),
             _ => new Vector2Int(cellA.x + 1, cellA.y),
         };
+        var cellC = direction switch
+        {
+            Direction.Up => new Vector2Int(cellA.x - 1, cellA.y),
+            Direction.Right => new Vector2Int(cellA.x, cellA.y + 1),
+            Direction.Down => new Vector2Int(cellA.x + 1, cellA.y),
+            Direction.Left => new Vector2Int(cellA.x, cellA.y - 1),
+            _ => new Vector2Int(cellA.x - 1, cellA.y),
+        };
+        var cellD = direction switch
+        {
+            Direction.Up => new Vector2Int(cellA.x - 2, cellA.y),
+            Direction.Right => new Vector2Int(cellA.x, cellA.y - 2),
+            Direction.Down => new Vector2Int(cellA.x + 2, cellA.y),
+            Direction.Left => new Vector2Int(cellA.x, cellA.y - 2),
+            _ => new Vector2Int(cellA.x - 2, cellA.y),
+        };
         List<Vector2Int> cells = new()
         {
             cellA,
-            cellB
+            cellB,
+            cellC,
+            cellD
         };
         return cells;
-    }  
+    } 
 }
--- Assets/Scripts/Selector/E5Selector.cs
+++ Assets/Scripts/Selector/E5Selector.cs
@@ -14,11 +14,38 @@ public class E5Selector : Selector
             Direction.Left => new Vector2Int(cellA.x, cellA.y + 1),
             _ => new Vector2Int(cellA.x + 1, cellA.y),
         };
+        var cellC = direction switch
+        {
+            Direction.Up => new Vector2Int(cellA.x - 1, cellA.y),
+            Direction.Right => new Vector2Int(cellA.x, cellA.y + 1),
+            Direction.Down => new Vector2Int(cellA.x + 1, cellA.y),
+            Direction.Left => new Vector2Int(cellA.x, cellA.y - 1),
+            _ => new Vector2Int(cellA.x - 1, cellA.y),
+        };
+        var cellD = direction switch
+        {
+            Direction.Up => new Vector2Int(cellA.x + 2, cellA.y),
+            Direction.Right => new Vector2Int(cellA.x, cellA.y - 2),
+            Direction.Down => new Vector2Int(cellA.x - 2, cellA.y),
+            Direction.Left => new Vector2Int(cellA.x, cellA.y + 2),
+            _ => new Vector2Int(cellA.x + 2, cellA.y),
+        };
+        var cellE = direction switch
+        {
+            Direction.Up => new Vector2Int(cellA.x - 2, cellA.y),
+            Direction.Right => new Vector2Int(cellA.x, cellA.y + 2),
+            Direction.Down => new Vector2Int(cellA.x + 2, cellA.y),
+            Direction.Left => new Vector2Int(cellA.x, cellA.y - 2),
+            _ => new Vector2Int(cellA.x - 2, cellA.y),
+        };
         List<Vector2Int> cells = new()
         {
             cellA,
-            cellB
+            cellB,
+            cellC,
+            cellD,
+            cellE
         };
         return cells;
-    }  
+    }
 }
