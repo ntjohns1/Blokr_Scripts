0a5af723afa7a2c4b8acdfb0a1bbf1403241ea75
ntjohns1 <nelsontjohns@gmail.com>
2024-02-15 21:54:15 -0500

    init refactor TileSelector

--- Assets/Scripts/Selector/TileSelector.cs
+++ Assets/Scripts/Selector/TileSelector.cs
@@ -14,17 +14,18 @@ namespace Blokr
 
         private Collider objectCollider;
 
+        private Vector3 currentRotation;
+
+
 
         [SerializeField] private LayerMask gridLayer;
 
-
-
-        // void Start ()
+        // void Start()
         // {
-        //     Vector2Int gridPoint = Geometry.GridPoint(0, 0);
-        //     Vector3 point = Geometry.PointFromGrid(gridPoint);
-        //     tileHighlight = Instantiate(tileHighlightPrefab, point, Quaternion.identity, gameObject.transform);
-        //     tileHighlight.SetActive(false);
+        //     if (tileHighlight != null)
+        //     {
+        //         currentRotation = tileHighlight.transform.eulerAngles;
+        //     }
         // }
 
         public void SetHighlight(GameObject highlightPrefab)
@@ -36,9 +37,7 @@ namespace Blokr
         {
             if (tileHighlight != null)
             {
-
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
                 RaycastHit hit;
                 if (Physics.Raycast(ray, out hit, 100f, gridLayer))
                 {
@@ -46,8 +45,8 @@ namespace Blokr
                     Vector2Int gridPoint = Geometry.GridFromPoint(point);
                     Piece piece = GameManager.Instance.SelectedPiece.GetComponent<Piece>();
                     int initDirection = (int)piece.PieceDirection;
-                    bool flipX = false;
-                    bool flipZ = false;
+                    // bool flipX = false;
+                    // bool flipZ = false;
 
                     int dirIndex = initDirection;
 
@@ -56,7 +55,8 @@ namespace Blokr
 
                     if (allPositionsInBounds)
                     {
-                        tileHighlight.transform.SetPositionAndRotation(Geometry.PointFromGrid(gridPoint), Quaternion.Euler(flipX ? 180.0f : 0.0f, 90.0f * (int)piece.PieceDirection, flipZ ? 180.0f : 0.0f));
+                        tileHighlight.transform.SetPositionAndRotation(Geometry.PointFromGrid(gridPoint), Quaternion.Euler(0.0f, 90.0f * (int)piece.PieceDirection, 0.0f));
+                        currentRotation = tileHighlight.transform.eulerAngles;
                         tileHighlight.SetActive(true);
                     }
                     else
@@ -64,17 +64,17 @@ namespace Blokr
                         tileHighlight.SetActive(false);
                     }
 
-                    Debug.Log($"GridPoint: {gridPoint}");
-                    int i = 0;
-                    foreach (Vector2Int item in occupiedGridPositions)
-                    {
-                        ++i;
-                        Debug.Log($"Cell{i}: {item}");
-                    }
+                    // Debug.Log($"GridPoint: {gridPoint}");
+                    // int i = 0;
+                    // foreach (Vector2Int item in occupiedGridPositions)
+                    // {
+                    //     ++i;
+                    //     Debug.Log($"Cell{i}: {item}");
+                    // }
 
                     if (Input.GetKeyUp("e"))
                     {
-                        tileHighlight.transform.Rotate(flipX ? 180.0f : 0.0f, 90.0f, flipZ ? 180.0f : 0.0f, Space.Self);
+                        tileHighlight.transform.Rotate(0.0f, 90.0f, 0.0f, Space.Self);
                         if (dirIndex < 3)
                         {
                             dirIndex++;
@@ -85,11 +85,12 @@ namespace Blokr
                         }
 
                         piece.PieceDirection = (Direction)dirIndex;
+                        currentRotation = tileHighlight.transform.eulerAngles;
                     }
 
                     if (Input.GetKeyUp("q"))
                     {
-                        tileHighlight.transform.Rotate(flipX ? 180.0f : 0.0f, -90.0f, flipZ ? 180.0f : 0.0f, Space.Self);
+                        tileHighlight.transform.Rotate(0.0f, -90.0f, 0.0f, Space.Self);
                         if (dirIndex > 0)
                         {
                             dirIndex--;
@@ -100,39 +101,73 @@ namespace Blokr
                         }
 
                         piece.PieceDirection = (Direction)dirIndex;
+                        currentRotation = tileHighlight.transform.eulerAngles;
                     }
 
                     if (Input.GetKeyUp("f"))
                     {
+                        // Log the initial rotation
+                        Debug.Log($"Initial Rotation: {currentRotation}");
+
+                        //     if (!piece.IsFlipped)
+                        //     {
+                        //         piece.IsFlipped = true;
+                        //         if ((int)piece.PieceType <= 4)
+                        //         {
+                        //             flipX = false;
+                        //             flipZ = true;
+
+                        //         }
+                        //         else
+                        //         {
+                        //             flipX = true;
+                        //             flipZ = false;
+                        //         }
+                        //     }
+                        //     else
+                        //     {
+                        //         piece.IsFlipped = false;
+                        //         flipX = false;
+                        //         flipZ = false;
+                        //     }
+
+                        //     // Reset rotation to identity before applying incremental rotation
+                        //     // tileHighlight.transform.rotation = Quaternion.identity;
+                        //     // Apply rotation incrementally after flipping
+                        //     tileHighlight.transform.Rotate(flipX ? 180.0f : 0.0f, 0.0f, flipZ ? 180.0f : 0.0f);
+                        //     currentRotation = tileHighlight.transform.eulerAngles;
+                        //     // Log the final rotation
+
                         if (!piece.IsFlipped)
                         {
-                            piece.IsFlipped = true;
                             if ((int)piece.PieceType <= 4)
                             {
-                                flipX = false;
-                                flipZ = true;
+                                tileHighlight.transform.Rotate(0.0f, 0.0f, 180.0f, Space.Self);
+                                currentRotation = tileHighlight.transform.eulerAngles;
                             }
                             else
                             {
-                                flipX = true;
-                                flipZ = false;
+                                tileHighlight.transform.Rotate(180.0f, 0.0f, 0.0f, Space.Self);
+                                currentRotation = tileHighlight.transform.eulerAngles;
                             }
+                            piece.IsFlipped = true;
                         }
                         else
                         {
                             piece.IsFlipped = false;
-                            flipX = false;
-                            flipZ = false;
                         }
+
+                        Debug.Log($"Final Rotation: {currentRotation}");
                     }
+
+
 
                     if (Input.GetMouseButtonUp(0))
                     {
-                        // List<Vector2Int> list = GetOccupiedCellsForType(piece.PieceType, piece.PieceDirection, gridPoint, tileHighlight);
-                        // foreach (Vector2Int i in list)
-                        // {
-                        //     Debug.Log(i);
-                        // }
+                        foreach (Vector2Int i in occupiedGridPositions)
+                        {
+                            Debug.Log(i);
+                        }
                         // GameManager.Instance.AddPiece(piece, tileHighlight);
                     }
 
@@ -146,7 +181,7 @@ namespace Blokr
                 }
             }
         }
-        
+
         private List<Vector2Int> GetOccupiedCellsForType(PieceType pieceType, Direction direction, bool isFlipped, Vector2Int gridPoint, GameObject tileHighlight)
         {
             List<Vector2Int> list = new List<Vector2Int>();
@@ -155,111 +190,111 @@ namespace Blokr
                 case PieceType.A1:
                     A1Selector a1Selector = tileHighlight.GetComponent<A1Selector>();
                     a1Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + a1Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + a1Selector.SelectorDirection);
                     return a1Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.A2:
                     A2Selector a2Selector = tileHighlight.GetComponent<A2Selector>();
                     a2Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + a2Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + a2Selector.SelectorDirection);
                     return a2Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.A3:
                     A3Selector a3Selector = tileHighlight.GetComponent<A3Selector>();
                     a3Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + a3Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + a3Selector.SelectorDirection);
                     return a3Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.A4:
                     A4Selector a4Selector = tileHighlight.GetComponent<A4Selector>();
                     a4Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + a4Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + a4Selector.SelectorDirection);
                     return a4Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.A5:
                     A5Selector a5Selector = tileHighlight.GetComponent<A5Selector>();
                     a5Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + a5Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + a5Selector.SelectorDirection);
                     return a5Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.B3:
                     B3Selector b3Selector = tileHighlight.GetComponent<B3Selector>();
                     b3Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + b3Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + b3Selector.SelectorDirection);
                     return b3Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.B4:
                     B4Selector b4Selector = tileHighlight.GetComponent<B4Selector>();
-                    Debug.Log("Selector Direction: " + b4Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + b4Selector.SelectorDirection);
                     b4Selector.SelectorDirection = direction;
                     return b4Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.B5:
                     B5Selector b5Selector = tileHighlight.GetComponent<B5Selector>();
                     b5Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + b5Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + b5Selector.SelectorDirection);
                     return b5Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.C4:
                     C4Selector c4Selector = tileHighlight.GetComponent<C4Selector>();
                     c4Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + c4Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + c4Selector.SelectorDirection);
                     return c4Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.C5:
                     C5Selector c5Selector = tileHighlight.GetComponent<C5Selector>();
                     c5Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + c5Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + c5Selector.SelectorDirection);
                     return c5Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.D4:
                     D4Selector d4Selector = tileHighlight.GetComponent<D4Selector>();
                     d4Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + d4Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + d4Selector.SelectorDirection);
                     return d4Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.D5:
                     D5Selector d5Selector = tileHighlight.GetComponent<D5Selector>();
                     d5Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + d5Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + d5Selector.SelectorDirection);
                     return d5Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.E4:
                     E4Selector e4Selector = tileHighlight.GetComponent<E4Selector>();
                     e4Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + e4Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + e4Selector.SelectorDirection);
                     return e4Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.E5:
                     E5Selector e5Selector = tileHighlight.GetComponent<E5Selector>();
                     e5Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + e5Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + e5Selector.SelectorDirection);
                     return e5Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.F5:
                     F5Selector f5Selector = tileHighlight.GetComponent<F5Selector>();
                     f5Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + f5Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + f5Selector.SelectorDirection);
                     return f5Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.G5:
                     G5Selector g5Selector = tileHighlight.GetComponent<G5Selector>();
                     g5Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + g5Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + g5Selector.SelectorDirection);
                     return g5Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.H5:
                     H5Selector h5Selector = tileHighlight.GetComponent<H5Selector>();
                     h5Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + h5Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + h5Selector.SelectorDirection);
                     return h5Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.I5:
                     I5Selector i5Selector = tileHighlight.GetComponent<I5Selector>();
                     i5Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + i5Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + i5Selector.SelectorDirection);
                     return i5Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.J5:
                     J5Selector j5Selector = tileHighlight.GetComponent<J5Selector>();
                     j5Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + j5Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + j5Selector.SelectorDirection);
                     return j5Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.K5:
                     K5Selector k5Selector = tileHighlight.GetComponent<K5Selector>();
                     k5Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + k5Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + k5Selector.SelectorDirection);
                     return k5Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 case PieceType.L5:
                     L5Selector l5Selector = tileHighlight.GetComponent<L5Selector>();
                     l5Selector.SelectorDirection = direction;
-                    Debug.Log("Selector Direction: " + l5Selector.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + l5Selector.SelectorDirection);
                     return l5Selector.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
                 default:
                     A1Selector a1 = tileHighlight.GetComponent<A1Selector>();
-                    Debug.Log("Selector Direction: " + a1.SelectorDirection);
+                    // Debug.Log("Selector Direction: " + a1.SelectorDirection);
                     return a1.GetOccupiedGridPositions(gridPoint, direction, isFlipped);
             }
         }
